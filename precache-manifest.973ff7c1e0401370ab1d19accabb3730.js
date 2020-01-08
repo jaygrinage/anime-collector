d969e88953792290cd9793302a51e73dd84a639c
jaygrinage <jonahgrinage@gmail.com>
2020-01-08 16:23:20 -0600

    Updates

---
+++ precache-manifest.973ff7c1e0401370ab1d19accabb3730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "677ba2c87cad55c0002581503ba99cd0",
+    "url": "/anime-collector/index.html"
+  },
+  {
+    "revision": "3607c8e1c6843a295ac9",
+    "url": "/anime-collector/static/css/main.0816f6a7.chunk.css"
+  },
+  {
+    "revision": "f32d648f995f5dab0bee",
+    "url": "/anime-collector/static/js/2.d7dd292f.chunk.js"
+  },
+  {
+    "revision": "3607c8e1c6843a295ac9",
+    "url": "/anime-collector/static/js/main.1cb81a5c.chunk.js"
+  },
+  {
+    "revision": "b438c35553022c028a71",
+    "url": "/anime-collector/static/js/runtime-main.b1b53378.js"
+  },
+  {
+    "revision": "346ef05c989ef6e9cf1dfc0f0743ff68",
+    "url": "/anime-collector/static/media/steins-gate.346ef05c.jpg"
+  }
+]);
